e351d0ec0224f3f1220fae8537c268ab61c93b60
Guillermo Snipe <guillermo@backbase.com>
2019-03-04 18:32:07 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bol-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bol-multi-select';
+}
