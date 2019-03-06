4a347970a7d14ac3683511094cc7138ab9835a1e
Guillermo Snipe <guillermo@backbase.com>
2019-03-06 17:55:17 +0100

    Data lazing loading complete

--- src/app/multiselect/multiselect.component.ts
+++ src/app/multiselect/multiselect.component.ts
@@ -9,7 +9,7 @@ import { CompileShallowModuleMetadata } from '@angular/compiler';
   styleUrls: ['./multiselect.component.scss']
 })
 export class MultiselectComponent implements OnInit {
-
+  // TODO: create an options object out of this...
   _pageSize = 100;
   _page = 1;
   _categories: ProductCategory[];
@@ -18,7 +18,7 @@ export class MultiselectComponent implements OnInit {
 
   constructor(private dataService: MultiSelectService, private fb: FormBuilder) {
     this.categoriesForm = this.fb.group({
-      categories: this.addCategories()
+      categories: this.fb.array([])
     });
   }
 
@@ -32,59 +32,75 @@ export class MultiselectComponent implements OnInit {
     return category.id;
   }
 
-  getBatchOfCategories(page: number = this._page, _pageSize: number = this._pageSize) {
+  // Adds a batch of categories to the array and returns the range of controls that must be created.
+  async addBatchOfCategories(page: number = this._page, _pageSize: number = this._pageSize) {
+    let categoriesInterval: Array<number>;
 
     // There are no more elements to add. Exit
-    if (this._categories.length - this.calculateInterval(page) <= (_pageSize * -1 + 1) ) {
-      return false;
+    if (this._categories.length - this.calculateCategoriesBatchLimit(page) <= (_pageSize * -1 + 1) ) {
+      return null;
     }
 
     this.categories.push( ...this._categories.filter(
       (item, index) =>
           (page > 1) ?
-              index >= this.calculateElementToStartFrom(page, this._pageSize)
-                && index < this.calculateInterval(page) : index < this.calculateInterval(page)
+              index >= this.calculateCategoryToStartFrom(page, this._pageSize) && index < this.calculateCategoriesBatchLimit(page)
+              : index < this.calculateCategoriesBatchLimit(page)
     ));
+
+    categoriesInterval = this.calculateCategoriesInterval(this.categories, page);
 
     this._page = this._page + 1;
     console.log(this.categories);
+    return categoriesInterval;
   }
 
-  calculateInterval(page, pageSize = this._pageSize) {
+  calculateCategoryToStartFrom(page = 1, pageSize = this._pageSize) {
+    let result = 0;
+    if (page > 1) { result = (page - 1) * pageSize; }
+
+    return result;
+  }
+
+  calculateCategoriesBatchLimit(page, pageSize = this._pageSize) {
     return page * pageSize;
   }
 
-  calculateElementToStartFrom(page = 1, pageSize = this._pageSize) {
-    return (page - 1) * pageSize;
+  calculateCategoriesInterval(categoriesArray, page: number, pageSize: number = this._pageSize) {
+    const start = this.calculateCategoryToStartFrom(page);
+    let finish = this.calculateCategoriesBatchLimit(page);
+
+    if (finish > categoriesArray.length) { finish = categoriesArray.length; }
+
+    return [start, finish];
   }
 
-  addCheckboxes() {
+  addCheckboxes( checkboxesToAdd: Array<number> | null ) {
+
+    if (checkboxesToAdd === null) { return false; }
+
+    const [start, end] = checkboxesToAdd;
     console.log('addCheckboxes');
-    this.categories.map( category => this.formCategories.push(this.fb.control(false)));
-  }
-
-  // not using it
-  addCategories() {
-    const categoriesArray = this.categories.map(category => {
-      return this.fb.control(category.selected);
-    });
-    return this.fb.array(categoriesArray);
+    console.log( `start: ${start} end: ${end}` );
+    // this.categories.map( category => this.formCategories.push(this.fb.control(category.selected)));
+    for (let i = start; i < end; i = i + 1) {
+      this.formCategories.push(this.fb.control(this.categories[i].selected));
+    }
   }
 
   ngOnInit(): void {
     this.dataService.categories$.subscribe(
       data => {
         this._categories = data;
-        this.getBatchOfCategories();
-        this.addCheckboxes();
+        this.addBatchOfCategories().then( checkboxes => this.addCheckboxes(checkboxes) );
       }
     );
   }
 
   onScrollingFinished() {
-    this.getBatchOfCategories();
+    this.addBatchOfCategories().then( checkboxes => this.addCheckboxes( checkboxes ));
     console.log('Scroll finished!');
-    //this.addCheckboxes();
+    // this.addCheckboxes();
   }
 
   onSubmit(formValue) {
